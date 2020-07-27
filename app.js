0769516e655524842c04ca1084b456b3b4047401
LamiaaQ <68843047+LamiaaQ@users.noreply.github.com>
2020-07-27 22:33:36 +0300

    [or.js] has been added, and used in app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
 const and = require('./and');
+const or = require('./or');
 let result = and([1, 0, 0, 1], [1, 1, 1, 0]);
+let result2 = or([1, 1, 1, 1], [1, 1, 1, 0]);
 console.log(result);
+console.log(result2);
